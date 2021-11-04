02d628ab1fc3d4763066cf552a83e9519e279678
vaeeloran <vaeeloran@gmail.com>
2021-11-04 01:30:40 -0400

    user

---
+++ WebApplication1/Controllers/userController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using MySql.Data.MySqlClient;
+
+using dto = WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+
+    public class TasksController : Controller
+    {
+        private MySqlDatabase MySqlDatabase { get; set; }
+        public TasksController(MySqlDatabase mySqlDatabase)
+        {
+            this.MySqlDatabase = mySqlDatabase;
+        }
+        private async Task<List<dto.user>> GetUsers()
+        {
+            var ret = new List<dto.user>();
+
+            var cmd = this.MySqlDatabase.Connection.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT userId, userName, userPsw FROM tbluser";
+
+
+            using (var reader = await cmd.ExecuteReaderAsync())
+                while (await reader.ReadAsync())
+                {
+                    var t = new dto.user()
+                    {
+                        userId = reader.GetFieldValue<int>(0),
+                        userName = reader.GetFieldValue<string>(1),
+                        userPsw = reader.GetFieldValue<string>(2)
+                    };
+
+
+
+                    ret.Add(t);
+                }
+            return ret;
+        }
+
+    }  
+}
+
---
+++ WebApplication1/Models/userIdentifier.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WebApplication1.Models
+{
+    public class userIdentifier
+    {
+        public int userId { get; set; }
+    }
+}
