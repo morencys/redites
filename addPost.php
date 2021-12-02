529d85b20a0744d1b5ab48fce2b43ebd9dc5caef
quiquideeps <gabygrenier2@yahoo.ca>
2021-12-02 18:29:52 -0500

    1

--- addPost.php
+++ addPost.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
     <html>
-        <p>salut</p>
+        <p>votre post a été envoyer</p>
+        <?php
+        require 'controller/HomeController.php';
+        $post = new showFrontpage(); 
+        $post->addPost();?>
+        <button class="btn btn-primary btn-dark btn-lg" type="button"onClick="document.location.href='view/FrontPage.php'" >Front Page of the internet</button>
     </html>
