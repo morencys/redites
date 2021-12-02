7af6fe6409873c37b125cd2a945a267b4b247a90
jean-francois <jean-francois.sicotte@hotmail.com>
2021-12-02 16:34:29 -0500

    30

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -7,24 +7,13 @@ class showFrontpage{
         $servername = "192.168.10.15";
         $username = "redites";
         $password = "Bingo123!";
-        $bdName = "tester";
+        $bdName = "redites";
 
         echo "test";
-        $conn = mysqli_connect($servername, $username, $password, $bdName);
+        $bdd = new PDO("mysql:host=$servername;dbname=$bdName;charset=utf8", $username, $password);
         echo "test2";
-        if (!$conn) {
-            die('Could not connect: ' . mysql_error());
-        }
-        echo 'Connected successfully';
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "test3";
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "test4";
-        $topic = $conn->query("SELECT * FROM tbltopic");
-        
-        while($topicInfo = $topic->fetch()){
-            echo $topicInfo['topicName'];
-        }
-
     }
 
     public function addPost(){
