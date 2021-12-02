69077a074d29688e8668551d439a0d5e71a261aa
jean-francois <jean-francois.sicotte@hotmail.com>
2021-12-02 15:34:57 -0500

    15

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -12,7 +12,7 @@ class showFrontpage{
         while($topicInfo = $topic->fetch()){
             echo $topicInfo['topicName'];
         }
-        dd($topic);
+        echo "test2";
     }
 
     public function addPost(){
