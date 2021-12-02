86f46ff1f0e419e0c4a195bb0ad344fae19d6aa8
quiquideeps <gabygrenier2@yahoo.ca>
2021-12-02 18:50:34 -0500

    1

--- addPost.php
+++ addPost.php
@@ -2,12 +2,11 @@
 <html lang="en">
     <html>
         <p>votre post a été envoyer</p>
+        <button class="btn btn-primary btn-dark btn-lg" type="button"onClick="document.location.href='/'" >Front Page of the internet</button>
         <?php
-        require 'controller/HomeController.php';
         $title = $_POST["title"];
         $topic = $_POST["topic"];
         $postTextArea = $_POST["postTextArea"];
         $post = new showFrontpage(); 
         $post->addPost();?>
-        <button class="btn btn-primary btn-dark btn-lg" type="button"onClick="document.location.href='/'" >Front Page of the internet</button>
     </html>
