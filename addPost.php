1b130225d8f64ffac7ae14e271cbe70b3bad6425
quiquideeps <gabygrenier2@yahoo.ca>
2021-12-02 18:47:46 -0500

    a

--- addPost.php
+++ addPost.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <html>
         <p>votre post a été envoyer</p>
-        <button class="btn btn-primary btn-dark btn-lg" type="button"onClick="document.location.href='52.60.127.121'" >Front Page of the internet</button>
+        <button class="btn btn-primary btn-dark btn-lg" type="button"onClick="document.location.href='/'" >Front Page of the internet</button>
         <?php
         require 'controller/HomeController.php';
         $title = $_POST["title"];
