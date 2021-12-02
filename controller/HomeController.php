1ef185bf471e08263f2a239892a33bd8379c4944
jean-francois <jean-francois.sicotte@hotmail.com>
2021-12-02 15:46:53 -0500

    19

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -1,15 +1,16 @@
 <?php
 class showFrontpage{
     public function showFrontPage(){
-        echo "test3";
         $servername = "192.168.10.15";
         $username = "redites";
         $password = "Bingo123!";
         $bdName = "redites";
+        echo "test3";
         $conn = new PDO("mysql:host=$servername;dbname=$bdName;charset=utf8", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "test4";
         $topic = $conn->query("SELECT * FROM tbltopic");
-        
+        echo "test5";
         while($topicInfo = $topic->fetch()){
             echo $topicInfo['topicName'];
         }
